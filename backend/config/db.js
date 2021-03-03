133dec940e35fa335789f19255571ce296b48cb1
Kevin Bacale <kevin.bacale@cybexai.com>
2021-03-03 19:05:48 +0100

    fetching all products from the api implimented

---
+++ backend/config/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    // const conn = await mongoose.connect(process.env.MONGO_URI, {
+    //     useUnifiedTopology: true,
+    //     useNewUrlParser: true,
+    //     useCreateIndex: true
+    // })
+
+    //using local db in development not to be always connected if using compass
+    const conn = await mongoose.connect("mongodb://localhost:27017/proshop", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (err) {
+    console.error(`Error: ${err.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+
+import productRoute from "./routes/productRoute.js";
+
+dotenv.config();
+
+//connecting to the DB
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is running..." });
+});
+
+app.use("/api/products", productRoute);
+
+
+//Handling wrong URL routes by sending back 404 status code 
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404);
+    next(error)
+})
+
+
+//custom express error handling
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
