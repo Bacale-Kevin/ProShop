d27713efe1ae18e75d023158cf62bca5e7cd39ac
Kevin Bacale <kevin.bacale@cybexai.com>
2021-03-05 15:11:14 +0100

    log in register user complited

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,33 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+//*@desc   Register a new user
+//*@route  POST /api/users
+//*@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error(" These user already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+  }
+});
+
 //*@desc   Get user profile
 //*@route  GET /api/users/profile
 //*@access Private
@@ -33,18 +60,19 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
   const user = await User.findById(req.user._id);
 
-
+  //* In these scenario when registering a user we assign generate a token immidiately
   if (user) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      token: generateToken(user._id),
     });
   } else {
     res.status(404);
-    throw new Error("User not found");
+    throw new Error("Invalid user data ");
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 const router = express.Router();
 
-import { authUser, getUserProfile } from "../controllers/userController.js";
-import { protect } from '../middleware/authMiddleware.js'
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/login").post(authUser);
 
-router.route("/profile").get(protect, getUserProfile)
+router.route("/profile").get(protect, getUserProfile);
+
+router.route("/").post(registerUser);
 
 export default router;
